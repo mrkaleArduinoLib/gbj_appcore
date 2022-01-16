22c67da4e02dde00ba97a8f1608a9a34f1334219
Libor Gabaj <libor.gabaj@gmail.com>
2022-01-16 11:46:05 +0100

    Added static constant NA and getter for boot reason.

--- src/gbj_appcore.cpp
+++ src/gbj_appcore.cpp
@@ -1,2 +1,3 @@
 #include "gbj_appcore.h"
 const String gbj_appcore::VERSION = "GBJ_APPCORE 1.0.0";
+const String gbj_appcore::NA = "n/a";
--- src/gbj_appcore.h
+++ src/gbj_appcore.h
@@ -31,6 +31,10 @@
 #elif defined(PARTICLE)
   #include <Particle.h>
 #endif
+extern "C"
+{
+#include "user_interface.h"
+}
 
 class gbj_appcore
 {
@@ -39,6 +43,7 @@ public:
   // Public constants
   //------------------------------------------------------------------------------
   static const String VERSION;
+  static const String NA;
   enum ResultCodes : byte
   {
     SUCCESS, // It should be always the first with index 0
@@ -52,19 +57,93 @@ public:
     ERROR_CONNECT, // Connection failed
     ERROR_PUBLISH, // Publishing failed
     ERROR_SUBSCRIBE, // Subsribing failed
+    ERROR_BOOT_DEFAULT_RST, // Normal startup by power on
+    ERROR_BOOT_WDT_RST, // Hardware watch dog reset
+    ERROR_BOOT_EXCEPTION_RST, // Exception reset
+    ERROR_BOOT_SOFT_WDT_RST, // Software watch dog reset
+    ERROR_BOOT_SOFT_RESTART, // Software restart, system_restart
+    ERROR_BOOT_DEEP_SLEEP_AWAKE, // Wake up from deep-sleep
+    ERROR_BOOT_EXT_SYS_RST, // External system reset (assertion of reset pin)
   };
 
-  //------------------------------------------------------------------------------
-  // Public setters - they usually return result code or void.
-  //------------------------------------------------------------------------------
-  inline ResultCodes setLastResult(ResultCodes lastResult = ResultCodes::SUCCESS)
+  /*
+    Rounding to particular decimal places.
+
+    DESCRIPTION:
+    The method rounds provided rational number to desired decimal places.
+
+    PARAMETERS:
+    value - Number to be rounded.
+      - Data type: float
+      - Default value: none
+      - Limited range: real numbers
+    prec - Precision, i.e., number of requested decimal places for a rounded value.
+      - Data type: byte
+      - Default value: none
+      - Limited range: 0 ~ 255
+
+    RETURN: Rounded real number.
+  */
+  inline float roundoff(float value, byte prec)
+  {
+    float pow_10 = pow(10.0f, (float)prec);
+    return round(value * pow_10) / pow_10; }
+
+  // Setters
+  inline ResultCodes setLastResult(
+    ResultCodes lastResult = ResultCodes::SUCCESS)
   {
     return _lastResult = lastResult;
   };
 
-  //------------------------------------------------------------------------------
-  // Public getters
-  //------------------------------------------------------------------------------
+  /*
+    Reset reason.
+
+    DESCRIPTION:
+    The method detects recent rebooting reason of the microcontroller.
+
+    PARAMETERS: None
+
+    RETURN: Result code
+  */
+  inline ResultCodes getResetReason()
+  {
+    setLastResult(ResultCodes::ERROR_UKNOWN);
+    switch (ESP.getResetInfoPtr()->reason)
+    {
+
+      case REASON_DEFAULT_RST:
+        setLastResult(ResultCodes::ERROR_BOOT_DEFAULT_RST);
+        break;
+
+      case REASON_WDT_RST:
+        setLastResult(ResultCodes::ERROR_BOOT_WDT_RST);
+        break;
+
+      case REASON_EXCEPTION_RST:
+        setLastResult(ResultCodes::ERROR_BOOT_EXCEPTION_RST);
+        break;
+
+      case REASON_SOFT_WDT_RST:
+        setLastResult(ResultCodes::ERROR_BOOT_SOFT_WDT_RST);
+        break;
+
+      case REASON_SOFT_RESTART:
+        setLastResult(ResultCodes::ERROR_BOOT_SOFT_RESTART);
+        break;
+
+      case REASON_DEEP_SLEEP_AWAKE:
+        setLastResult(ResultCodes::ERROR_BOOT_DEEP_SLEEP_AWAKE);
+        break;
+
+      case REASON_EXT_SYS_RST:
+        setLastResult(ResultCodes::ERROR_BOOT_EXT_SYS_RST);
+        break;
+    }
+    return getLastResult();
+  }
+
+  // Getters
   inline ResultCodes getLastResult() { return _lastResult; };
   inline bool isSuccess() { return _lastResult == ResultCodes::SUCCESS; };
   inline bool isError() { return !isSuccess(); };
